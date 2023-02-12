dcf164292f09aa7865f3c2c6eac3710683ac41c3
Mihály Zsolt <zsolt.mihaly@optasoft.hu>
2023-02-12 18:03:29 +0100

    Browser template added.

--- src/app.py
+++ src/app.py
@@ -29,23 +29,37 @@ class TagRelation(db.Model):
     id = db.Column(db.Integer, db.Sequence("tag_relation_id_seq", start=1), primary_key=True)
     parent_tag = db.Column(db.Integer, db.ForeignKey('tag.id'), nullable=False)
     child_tag = db.Column(db.Integer, db.ForeignKey('tag.id'), nullable=False)
+    def __repr__(self):
+        return f"{self.id} {self.parent_tag} {self.child_tag}"
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    tags = Tag.query.filter_by(index_tag=True)
+    return render_template('index.html', tags=tags)
 
+
+@app.route('/browser/<int:id>')
+def browser(id):
+    child_tag_ids = TagRelation.query.with_entities(TagRelation.child_tag).filter(TagRelation.parent_tag == id).all()
+
+    ids = set()
+    for set_in_list in child_tag_ids:
+        for element in set_in_list:
+            ids.add(element)
+
+    child_tags = Tag.query.filter(Tag.id.in_(ids)).all()
+    return render_template('browser.html', tags=child_tags)
 
 
 @app.route('/tags')
 def tags():
     tags = Tag.query.all()
-    print(tags)
     return render_template('tags.html', tags=tags)
 
 
 @app.route('/add_tag', methods=["POST"])
-def tag():
+def add_tag():
     # In this function we will input data from the
     # form page and store it in our database.
     # Remember that inside the get the name should
@@ -64,6 +78,14 @@ def tag():
         return redirect('/tags')
 
 
+@app.route('/make_index_tag/<int:id>')
+def make_index_tag(id):
+    tag = Tag.query.get(id)
+    tag.index_tag = True
+    db.session.commit()
+    return redirect('/tags')
+
+
 
 
 @app.route('/delete_tag/<int:id>')
@@ -79,16 +101,17 @@ def delete_tag(id):
 @app.route('/tag_relations')
 def tag_relations():
     tags = Tag.query.all()
-    print(tags)
     tag_relations = TagRelation.query.all()
     return render_template('tag_relations.html', tags=tags, tag_relations=tag_relations)
 
 
 @app.route('/add_tag_relation', methods=["POST"])
 def add_tag_relation():
-    parent_tag = request.form.get("parent_tag")
-    child_tag = request.form.get("child_tag")
-    tag_relation = TagRelation(parent_tag=parent_tag, child_tag=child_tag)
+    parent_tag_content = request.form.get("parent_tag")
+    child_tag_content = request.form.get("child_tag")
+    parent_tag = Tag.query.filter_by(content=parent_tag_content).first()
+    child_tag = Tag.query.filter_by(content=child_tag_content).first()
+    tag_relation = TagRelation(parent_tag=parent_tag.id, child_tag=child_tag.id)
     db.session.add(tag_relation)
     db.session.commit()
     return redirect('/tag_relations')
